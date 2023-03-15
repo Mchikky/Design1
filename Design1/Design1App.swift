9e2441fb3454d034310fff6f31203a8cea2f5914
Chikaodili Deng <mchikky@yahoo.com>
2023-03-15 09:26:45 +0100

    Initial Commit

---
+++ Design1/Design1App.swift
@@ -0,0 +1,17 @@
+//
+//  Design1App.swift
+//  Design1
+//
+//  Created by Chikaodili Deng on 15/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Design1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
