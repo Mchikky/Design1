16eda8c90ad65a43782562f4f7a21e6685ff2ec6
Chikaodili Deng <mchikky@yahoo.com>
2023-03-17 15:19:10 +0100

    Constantes, variables et data types

--- Design1/ContentView.swift
+++ Design1/ContentView.swift
@@ -10,14 +10,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("My first SwiftUI App")
+                .font(.title)
+        Spacer()
+            Text ("Sweet ❤️")
+                .foregroundColor(.orange)
+        Spacer()
         }
-        .padding()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
